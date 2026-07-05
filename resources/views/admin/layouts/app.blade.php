081f36cda01401eb793593f4402dc557ec2dedc2
Nguyen-Theanh <cpheo666@gmail.com>
2026-07-05 15:20:58 +0700

    .

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -42,7 +42,7 @@
             </a>
 
             <nav class="nav flex-column gap-2">
-                <a class="nav-link {{ request()->routeIs('admin.products.*') ? 'active' : '' }}" href="{{ route('admin.products.index') }}">Quản lý san pham</a>
+                <a class="nav-link {{ request()->routeIs('admin.products.*') ? 'active' : '' }}" href="{{ route('admin.products.index') }}">Quản lý sản phẩm</a>
                 <a class="nav-link {{ request()->routeIs('admin.categories.*') ? 'active' : '' }}" href="{{ route('admin.categories.index') }}">Quản lý danh mục</a>
                 <a class="nav-link {{ request()->routeIs('admin.users.*') ? 'active' : '' }}" href="{{ route('admin.users.index') }}">Quản lý người dùng</a>
                 <a class="nav-link" href="{{ route('client.products.index') }}">Xem trang client</a>
